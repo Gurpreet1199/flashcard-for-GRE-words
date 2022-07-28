c06175d7613f2777ebb7d81faa447a9f03849228
Gurpreet1199 <42606638+Gurpreet1199@users.noreply.github.com>
2022-07-28 17:57:48 +0530

    Update flashcard.py

--- flashcard.py
+++ flashcard.py
@@ -8,18 +8,22 @@ re={}
 delim= "->"
 for line in myfile:
     data= line.strip().split(delim)
-    s=data[0]
-    s=s[0:-1]
-    data[0]=s
-    data_dict[data[0]]=np.array(data[1:])       
- 
+    s=np.array(data[1:])
+    cs=""
+    for x in s:
+        cs=cs+x
+    cs=cs.strip()
+    data[0]=data[0].strip()
+    data_dict[data[0]]=cs
 myfile.close()
 print("       n for next and m for meaning\n\n\n")
 while(True):
+    if len(data_dict)==0:
+        break
+    print("remaining words ",len(data_dict))
     word,meaning =random.choice(list(data_dict.items()))
     print(word)
     option=input("s-> search word by yourself, enter->generate next word , m-> meaning , q-> quit\n")
-
     if(option == 'm'):
         re[word]=meaning
         print("MEANING OF THE WORD IS -> ",meaning)
@@ -28,25 +32,16 @@ while(True):
         x=input("enter word with a space in the end-> ")
         if x not in data_dict:
             print("Word Not Found,, Please enter correct word\n\n")
-            continue
-        
+            continue       
         print(data_dict[x])
-
         print("\n")
-
     elif(option=='q'):
        break
     elif(option not in 's','m','q'):
         del data_dict[word]
-
-print("\n\n\t\t\t press b to build new file with words seem more tough else q to quit\n\n")
-
+print("\n\n\t\t\t press 'b' to build new file with words seem more tough else 'q' to quit\n\n")
 op= input('enter choice : ')
-
 if(op=='b'):
     with open("retry.txt",'w') as f:
         for k,v in re.items():
             f.write('%s -> %s\n'%(k,v))
-
-
- 
