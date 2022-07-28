f41f61afb1a87b6dd18952b27ac7c09fcdc2b1e2
Gurpreet1199 <42606638+Gurpreet1199@users.noreply.github.com>
2022-07-28 12:35:17 +0530

    Update flashcard.py

--- flashcard.py
+++ flashcard.py
@@ -1,10 +1,16 @@
 import numpy as np
 import random
-myfile = open("Word list gre.txt", 'r')
+import sys
+f=sys.argv[1]
+myfile = open(f, 'r')
 data_dict = {}
+re={}
 delim= "->"
 for line in myfile:
-    data= line.strip().split("->")
+    data= line.strip().split(delim)
+    s=data[0]
+    s=s[0:-1]
+    data[0]=s
     data_dict[data[0]]=np.array(data[1:])       
  
 myfile.close()
@@ -12,10 +18,10 @@ print("       n for next and m for meaning\n\n\n")
 while(True):
     word,meaning =random.choice(list(data_dict.items()))
     print(word)
-    option=input("s-> search word by yourself, n->generate next word , m-> meaning , q-> quit\n")
-    if(option=='n'):
-        continue
-    elif(option == 'm'):
+    option=input("s-> search word by yourself, enter->generate next word , m-> meaning , q-> quit\n")
+
+    if(option == 'm'):
+        re[word]=meaning
         print("MEANING OF THE WORD IS -> ",meaning)
         print("\n\n")
     elif(option=='s'):
@@ -30,7 +36,17 @@ while(True):
 
     elif(option=='q'):
        break
+    elif(option not in 's','m','q'):
+        del data_dict[word]
 
+print("\n\n\t\t\t press b to build new file with words seem more tough else q to quit\n\n")
+
+op= input('enter choice : ')
+
+if(op=='b'):
+    with open("retry.txt",'w') as f:
+        for k,v in re.items():
+            f.write('%s -> %s\n'%(k,v))
 
 
  
